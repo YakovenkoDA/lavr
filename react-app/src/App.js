39dd142a512cbe0b6dedbe1d1cf11196cf565b6e
Yakovenko Dmitriy <d.yakovenko@mobidev.biz>
2019-01-13 19:40:58 +0200

    try Redux

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,30 +2,40 @@ import React, { Component } from 'react';
 import ArticleList from './components/ArticleList'
 import db from './dbArticles'
 
+import {createStore} from 'redux';
+
+const initialState = {reverted: false};
+
+function reducer (state = {reverted: false}, action){
+    switch (action.type){
+        case 'REVERT': return { reverted: !state.reverted};
+        default: return state;
+    }
+}
+
+const store = createStore(reducer, initialState);
+
 class App extends Component {
-    state = {
-        reverted: false
-    };
+    componentDidMount() {
+        store.subscribe(() => this.forceUpdate());
+    }
 
     render() {
+        const reverted = store.getState().reverted;
         return (
             <div className="App">
                 <h1>
-                    App Name
+                    App Name {reverted}
                     <button onClick={this.revert}>Revert</button>
                 </h1>
                 <ArticleList articles={
-                    this.state.reverted
-                        ? db.slice().reverse()
-                        : db
+                    reverted ? db.slice().reverse() : db
                 }/>
             </div>
         );
     }
 
-    revert = () => this.setState({
-        reverted: !this.state.reverted
-    })
+    revert = () => store.dispatch({type: 'REVERT'})
 }
 
 export default App;
